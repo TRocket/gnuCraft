df7168904c95c68f67dd909a0918d9afcaf25ed7
TRocket <TRocket@Itchypp.github>
2013-04-18 18:03:49 +0100

    added entry point file and version

---
+++ src/gnucraft.cpp
@@ -0,0 +1,15 @@
+/*
+ * gnucraft.cpp
+ *
+ *  Created on: 18 Apr 2013
+ *      Author: TRocket
+ */
+#include <iostream>
+#include "version.h"
+
+int main(void) {
+	std::cout << "gnucraft v." << GIT_VERSION << std::endl;
+}
+
+
+
---
+++ src/version.h
@@ -0,0 +1,15 @@
+/*
+ * version.h
+ *
+ *  Created on: 18 Apr 2013
+ *      Author: TRocket
+ */
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+//#ifdef __GIT_VERSION
+const std::string GIT_VERSION = __GIT_VERSION;
+//#endif
+
+#endif /* VERSION_H_ */
